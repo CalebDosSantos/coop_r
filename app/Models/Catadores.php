d1eec5d97003ffe8ec3ca8a07db8c404a4ffd22b
Caleb DSS <caleb.santos.dev@gmail.com>
2022-01-19 10:59:57 -0300

    First Commit

---
+++ app/Models/Catadores.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Catadores extends Model
+{
+    use HasFactory;
+}
